a8f7f1ceeadc6e3fcfd031525efb15eeab7ad47f
coyotew1 <55400703+coyotew1@users.noreply.github.com>
2020-03-02 09:12:07 -0700

    added method

--- Main.java
+++ Main.java
@@ -4,4 +4,10 @@ public class Main
     {
         System.out.println("STRT");
     }
+
+    public boolean scoresIncreasing(int[] scores) {
+        for (int i = 0; i <= scores.length - 2; i++)
+            if (scores[i] > scores[i + 1]) return false;
+        return true;
+    }
 }
